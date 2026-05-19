495d7c538fb6892a3bf094912acaa793590ab33d
rhit-bih2 <bih2@rose-hulman.edu>
2026-05-19 16:21:03 -0400

    Create TradeDetailService

---
+++ src/yot/services/TradeDetailService.java
@@ -0,0 +1,11 @@
+package yot.services;
+
+public class TradeDetailService {
+	private DatabaseConnectionService dbService = null;
+	
+	public TradeDetailService(DatabaseConnectionService dbService) {
+		this.dbService = dbService;
+	}
+	
+	
+}
