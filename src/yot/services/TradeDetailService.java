aa2ad4f1aa53e8291dea7c4ebcc40a1856f5ff03
rhit-kimy8 <kimy8@rose-hulman.edu>
2026-05-19 16:45:24 -0400

    Tradedetailservice draft

--- src/yot/services/TradeDetailService.java
+++ src/yot/services/TradeDetailService.java
@@ -1,11 +1,97 @@
 package yot.services;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import yot.services.TradeService.TradeRequest;
+
 public class TradeDetailService {
 	private DatabaseConnectionService dbService = null;
 	
-	public TradeDetailService(DatabaseConnectionService dbService) {
+	public TradeDetailService(DatabaseConnectionService dbService, String username) {
 		this.dbService = dbService;
 	}
 	
+	public static class Card {
+		private final int id;
+		private final String name;
+		private final int quantity;
+
+		public Card(int id, String name, int quantity) {
+			this.id = id;
+			this.name = name;
+			this.quantity = quantity;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+		
+		public int getQuantity() {
+			return quantity;
+		}
+	}
+	
+	public Map<String, String> GetTradeConfirmInfo(int tradeID){
+		Map<String, String> map = new HashMap<String,String>();
+		Connection con = dbService.getConnection();
+		CallableStatement cs = null;
+		String query = "{Call GetTradeInfo(?)}";
+		
+		try {
+			cs = con.prepareCall(query);
+			cs.setInt(1, tradeID);
+			ResultSet rs = cs.executeQuery();
+			if (rs.next()) {
+				map.put("SenderConfirmed", rs.getString("SenderConfirmed"));
+				map.put("ReceiverConfirmed", rs.getString("ReceiverConfirmed"));
+				map.put("IsComplete", rs.getString("IsComplete"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return map;
+	}
+	
+	public Map<String, String> GetTradeAllInfo(int tradeID){ //Not yet done
+		Map<String, String> map = new HashMap<String,String>();
+		Connection con = dbService.getConnection();
+		CallableStatement cs = null;
+		String query = "{Call GetTradeInfo(?)}";
+		
+		try {
+			cs = con.prepareCall(query);
+			cs.setInt(1, tradeID);
+			ResultSet rs = cs.executeQuery();
+			if (rs.next()) {
+				map.put("SenderConfirmed", rs.getString("SenderConfirmed"));
+				map.put("ReceiverConfirmed", rs.getString("ReceiverConfirmed"));
+				map.put("IsComplete", rs.getString("IsComplete"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return map;
+	}
+	
+	public ArrayList<Card> GetCardsOffered(int tradeID, String username){
+		return null;
+	}
+	
+	public boolean OfferCard(int tradeID, String username, int cardID) {
+		
+		
+		return false;
+	}
 	
 }
