19ab867216b1b089ddbd55e8258341777dac608a
rhit-kimy8 <kimy8@rose-hulman.edu>
2026-05-20 14:06:14 -0400

    TradeDetailService

--- src/yot/services/TradeDetailService.java
+++ src/yot/services/TradeDetailService.java
@@ -2,11 +2,14 @@ package yot.services;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import yot.services.TradeService.TradeRequest;
 
 public class TradeDetailService {
 	private DatabaseConnectionService dbService = null;
@@ -50,9 +53,11 @@ public class TradeDetailService {
 			cs.setInt(1, tradeID);
 			ResultSet rs = cs.executeQuery();
 			if (rs.next()) {
+				map.put("ID", rs.getString("ID"));
+				map.put("IsComplete", rs.getString("IsComplete"));
 				map.put("SenderConfirmed", rs.getString("SenderConfirmed"));
 				map.put("ReceiverConfirmed", rs.getString("ReceiverConfirmed"));
-				map.put("IsComplete", rs.getString("IsComplete"));
+				map.put("DateCreated", rs.getString("DateCreated"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -60,20 +65,24 @@ public class TradeDetailService {
 		return map;
 	}
 	
-	public Map<String, String> getAllTradeInfo(int tradeID){ //Not yet done
+	public Map<String, String> getAllTradeInfo(int tradeID){ 
 		Map<String, String> map = new HashMap<String,String>();
 		Connection con = dbService.getConnection();
 		CallableStatement cs = null;
-		String query = "{Call GetTradeInfo(?)}";
+		String query = "{Call GetAllTradeInfo(?)}";
 		
 		try {
 			cs = con.prepareCall(query);
 			cs.setInt(1, tradeID);
 			ResultSet rs = cs.executeQuery();
-			if (rs.next()) {
+			if (rs.next()) {	
+				map.put("ID", rs.getString("ID"));
+				map.put("IsComplete", rs.getString("IsComplete"));
 				map.put("SenderConfirmed", rs.getString("SenderConfirmed"));
 				map.put("ReceiverConfirmed", rs.getString("ReceiverConfirmed"));
-				map.put("IsComplete", rs.getString("IsComplete"));
+				map.put("DateCreated", rs.getString("DateCreated"));
+				map.put("SenderUsername", rs.getString("SenderUsername"));
+				map.put("ReceiverUsername", rs.getString("ReceiverUsername"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -82,44 +91,150 @@ public class TradeDetailService {
 	}
 	
 	public ArrayList<Card> getCardsOffered(int tradeID, String username){
+		ArrayList<Card> list = new ArrayList<Card>();
+		Connection con = dbService.getConnection();
+		CallableStatement cs = null;
+		String query = "{Call GetCardsOffered(?,?)}";
 		
-		return null;
+		try {
+			cs = con.prepareCall(query);
+			cs.setInt(1, tradeID);
+			cs.setString(2, username);
+			ResultSet rs = cs.executeQuery();
+			while (rs.next()) {
+				int cardId = rs.getInt("CardID");
+				String cardName = rs.getString("CardName");
+				int quantity = rs.getInt("CardQuantity");
+				list.add(new Card(cardId, cardName, quantity));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
 	}
 	
 	public boolean offerCard(int tradeID, int cardID, String username) {
+		Connection con = dbService.getConnection();
+		CallableStatement cs = null;
+		String query = "{Call OfferCard(?,?,?)}";
 		
-		return false;
+		try {
+			cs = con.prepareCall(query);
+			cs.setInt(1, tradeID);
+			cs.setInt(2, cardID);
+			cs.setString(3, username);
+			cs.execute();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
 	public boolean incrementCardOffered(int tradeID, int cardID, String username) {
-
-		return false;
+		Connection con = dbService.getConnection();
+		CallableStatement cs = null;
+		String query = "{Call IncrementCardOffered(?,?,?)}";
+		
+		try {
+			cs = con.prepareCall(query);
+			cs.setInt(1, tradeID);
+			cs.setInt(2, cardID);
+			cs.setString(3, username);
+			cs.execute();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
 	public boolean decrementCardOffered(int tradeID, int cardID, String username) {
-
-		return false;
+		Connection con = dbService.getConnection();
+		CallableStatement cs = null;
+		String query = "{Call DecrementCardOffered(?,?,?)}";
+		
+		try {
+			cs = con.prepareCall(query);
+			cs.setInt(1, tradeID);
+			cs.setInt(2, cardID);
+			cs.setString(3, username);
+			cs.execute();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
 	public boolean removeCardOffered(int tradeID, int cardID, String username) {
+		Connection con = dbService.getConnection();
+		CallableStatement cs = null;
+		String query = "{Call RemoveCardOffered(?,?,?)}";
 		
-		return false;
+		try {
+			cs = con.prepareCall(query);
+			cs.setInt(1, tradeID);
+			cs.setInt(2, cardID);
+			cs.setString(3, username);
+			cs.execute();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
 	public String getCardImage(int cardID) {
+		String imageURL = null;
+		Connection con = dbService.getConnection();
+		CallableStatement cs = null;
+		String query = "{Call GetCardImage(?)}";
 		
-		return null;
+		try {
+			cs = con.prepareCall(query);
+			cs.setInt(1, cardID);
+			ResultSet rs = cs.executeQuery();
+			if (rs.next()) {
+				imageURL = rs.getString("ImageURL");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return imageURL;
 	}
 	
 	public boolean confirmTrade(int tradeID, String username) {
+		Connection con = dbService.getConnection();
+		CallableStatement cs = null;
+		String query = "{Call ConfirmTrade(?,?)}";
 		
-		return false;
+		try {
+			cs = con.prepareCall(query);
+			cs.setInt(1, tradeID);
+			cs.setString(2, username);
+			cs.execute();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
 	public boolean abortTrade(int tradeID) {
+		Connection con = dbService.getConnection();
+		CallableStatement cs = null;
+		String query = "{Call DeleteTradeRequest(?)}";
 		
-		return false;
+		try {
+			cs = con.prepareCall(query);
+			cs.setInt(1, tradeID);
+			cs.execute();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
-	
 	
 }
